63616b6afb7dbcae2d46ed12cf561b69a029108a
Shivam Tewari <43674175+ShiviPro@users.noreply.github.com>
2025-05-15 19:09:17 +0530

    fix: add copyright text& modify footer appearance

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 const Footer = () => {
   return (
-    <footer className="mt-5">
+    <footer className="mt-5 bg-dark text-light py-3">
       <div className="container">
-        <p>
+        <p className="opacity-75">
           Flower icons created by{" "}
-          <a target="_blank" href="https://www.flaticon.com/authors/gungyoga04">
+          <a
+            target="_blank"
+            href="https://www.flaticon.com/authors/gungyoga04"
+            className="text-decoration-none"
+          >
             gungyoga04
           </a>{" "}
           -{" "}
@@ -12,9 +16,13 @@ const Footer = () => {
             target="_blank"
             href="https://www.flaticon.com/free-icons/flower"
             title="flower icons"
+            className="text-decoration-none"
           >
             Flaticon
           </a>
+        </p>
+        <p className="mb-0 opacity-75">
+          &copy; Mizuki Hanae. All rights reserved.
         </p>
       </div>
     </footer>
