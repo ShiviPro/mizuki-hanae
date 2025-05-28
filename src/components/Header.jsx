8c8beecb8f7864b75bc6b96f4aa4d139ae7ced8e
Shivam Tewari <43674175+ShiviPro@users.noreply.github.com>
2025-05-28 18:11:30 +0530

    fix: navbar brand linked to homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="position-fixed w-100 top-0 z-1">
       <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
         <div className="container">
-          <a href="" className="navbar-brand">
+          <NavLink to="/" className="navbar-brand">
             Mizuki Hanae
-          </a>
+          </NavLink>
 
           <button
             className="navbar-toggler"
@@ -28,29 +30,29 @@ const Header = () => {
             </div>
             <ul className="navbar-nav text-center">
               <li className="nav-item mx-3">
-                <a className="nav-link" href="">
+                <NavLink className="nav-link" to="">
                   <span className="btn btn-primary">Login</span>
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item mx-3">
-                <a className="nav-link" href="">
+                <NavLink className="nav-link" to="">
                   <span className="bi bi-heart fs-4 position-relative">
                     <span className="badge text-bg-danger px-1 py-0 rounded-pill position-absolute start-100 bottom-50 translate-middle-x">
                       <small>0</small>
                     </span>
                   </span>{" "}
                   <span className="ms-2">Wishlist</span>
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item mx-3">
-                <a className="nav-link" href="">
+                <NavLink className="nav-link" to="">
                   <span className="bi bi-cart3 fs-4 position-relative">
                     <span className="badge text-bg-danger px-1 py-0 rounded-pill position-absolute start-100 bottom-50 translate-middle-x">
                       <small>0</small>
                     </span>
                   </span>{" "}
                   <span className="ms-2">Cart</span>
-                </a>
+                </NavLink>
               </li>
             </ul>
           </div>
