ce6957bd31a137753c43b7c7b6a21ca2336911a2
Shivam Tewari <43674175+ShiviPro@users.noreply.github.com>
2025-06-03 22:54:39 +0530

    fix: spacing & width on product details page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -14,110 +14,112 @@ const ProductDetails = () => {
     <>
       <Header />
       <main className="mt-5 pb-5 bg-body-tertiary">
-        <section className="row text-center">
+        <section className="text-center">
           <h2 className="py-5 mb-0 display-5">{product.name}</h2>
-          <div className="col-md-3">
-            <section className="mb-4">
-              <h5>Artist</h5>
-              <img
-                src={product.artist.profilePic}
-                alt={product.artist.name}
-                className="w-50 rounded-circle border border-light-subtle border-4"
-              />
-              <p className="mb-0 fs-5 fw-semi-bold">{product.artist.name}</p>
-              <button className="btn btn-link pt-1 pe-1 text-dark">
-                + Follow
-              </button>
-              <span className="text-secondary">
-                ({product.artist.noOfFollowers} fans)
-              </span>
-            </section>
-            <section>
-              <h5>
-                {product.name} ({product.yearOfMaking.getFullYear()})
-              </h5>
-              <p className="text-secondary mb-1">
-                {product.additionalInfo.medium}
-              </p>
-              <p className="mb-5">{product.description}</p>
-
-              <p className="mb-1">
-                <span className="text-secondary">Dimensions:</span>{" "}
-                {product.dimensions}
-              </p>
-              <p className="mb-1">
-                <span className="text-secondary">Framing:</span>{" "}
-                {product.isFramed ? "Yes" : "No"}
-              </p>
-              <p className="mb-1">
-                <span className="text-secondary">State of Preservation:</span>{" "}
-                {product.stateOfPreservation}
-              </p>
-              <p className="mb-5">
-                <span className="text-secondary">Guarantee:</span>{" "}
-                {product.proofOfAuthenticity}
-              </p>
-
-              <p className="mb-1">Tags:</p>
-              {product.tags.map((tag) => (
-                <button
-                  key={tag}
-                  className="my-2 mx-1 btn btn-text text-black bg-secondary-subtle rounded-pill text-light"
-                >
-                  {tag}
-                </button>
-              ))}
-            </section>
-          </div>
-          <div className="col-md-6">
-            <section className="position-relative">
-              <img
-                className="w-100 img-fluid"
-                src={product.images[0]}
-                alt={product.name}
-              />
-              <span className="bi bi-heart position-absolute end-0 mt-2 me-3 fs-5 fw-bold text-danger"></span>
-            </section>
-            <section className="row mt-3">
-              {product.images.map((image, index) => (
+          <div className="row w-100">
+            <div className="col-md-3">
+              <section className="mb-4 px-3">
+                <h5>Artist</h5>
                 <img
-                  key={image}
-                  className="col-6 w-25 img-fluid"
-                  src={image}
-                  alt={`${product.name}'s image ${index + 1}`}
+                  src={product.artist.profilePic}
+                  alt={product.artist.name}
+                  className="w-50 rounded-circle border border-light-subtle border-4"
                 />
-              ))}
-            </section>
-          </div>
-          <div className="col-md-3">
-            <section>
-              <h6>Sold & Delivered by</h6>
-              <p className="mb-3">{product.delivery.by}</p>
-              <p className="fs-5 fw-semi-bold mb-2">₹{product.price}</p>
-              <p className="mb-1">
-                <small>+₹{product.delivery.charge} delivery charge</small>
-              </p>
-              <p className="mb-1">
-                <small>
-                  Delivery by{" "}
-                  {product.delivery.estimatedDate.toLocaleDateString()}
-                </small>
-              </p>
-              <p>
-                <small>
-                  {product.delivery.type} from {product.delivery.from.city} -{" "}
-                  {product.delivery.from.country}
-                </small>
-              </p>
-            </section>
-            <section className="mt-5">
-              <button className="btn btn-outline-primary rounded-pill px-4 me-2">
-                Add to Cart
-              </button>
-              <button className="btn btn-primary rounded-pill px-4">
-                Buy Now
-              </button>
-            </section>
+                <p className="mb-0 fs-5 fw-semi-bold">{product.artist.name}</p>
+                <button className="btn btn-link pt-1 pe-1 text-dark">
+                  + Follow
+                </button>
+                <span className="text-secondary">
+                  ({product.artist.noOfFollowers} fans)
+                </span>
+              </section>
+              <section className="px-3">
+                <h5>
+                  {product.name} ({product.yearOfMaking.getFullYear()})
+                </h5>
+                <p className="text-secondary mb-1">
+                  {product.additionalInfo.medium}
+                </p>
+                <p className="mb-5">{product.description}</p>
+
+                <p className="mb-1">
+                  <span className="text-secondary">Dimensions:</span>{" "}
+                  {product.dimensions}
+                </p>
+                <p className="mb-1">
+                  <span className="text-secondary">Framing:</span>{" "}
+                  {product.isFramed ? "Yes" : "No"}
+                </p>
+                <p className="mb-1">
+                  <span className="text-secondary">State of Preservation:</span>{" "}
+                  {product.stateOfPreservation}
+                </p>
+                <p className="mb-5">
+                  <span className="text-secondary">Guarantee:</span>{" "}
+                  {product.proofOfAuthenticity}
+                </p>
+
+                <p className="mb-1">Tags:</p>
+                {product.tags.map((tag) => (
+                  <button
+                    key={tag}
+                    className="my-2 mx-1 btn btn-text text-black bg-secondary-subtle rounded-pill text-light"
+                  >
+                    {tag}
+                  </button>
+                ))}
+              </section>
+            </div>
+            <div className="col-md-6">
+              <section className="position-relative">
+                <img
+                  className="w-100 img-fluid"
+                  src={product.images[0]}
+                  alt={product.name}
+                />
+                <span className="bi bi-heart position-absolute end-0 mt-2 me-3 fs-5 fw-bold text-danger"></span>
+              </section>
+              <section className="row mt-3">
+                {product.images.map((image, index) => (
+                  <img
+                    key={image}
+                    className="col-6 w-25 img-fluid"
+                    src={image}
+                    alt={`${product.name}'s image ${index + 1}`}
+                  />
+                ))}
+              </section>
+            </div>
+            <div className="col-md-3">
+              <section className="px-3">
+                <h6>Sold & Delivered by</h6>
+                <p className="mb-3">{product.delivery.by}</p>
+                <p className="fs-5 fw-semi-bold mb-2">₹{product.price}</p>
+                <p className="mb-1">
+                  <small>+₹{product.delivery.charge} delivery charge</small>
+                </p>
+                <p className="mb-1">
+                  <small>
+                    Delivery by{" "}
+                    {product.delivery.estimatedDate.toLocaleDateString()}
+                  </small>
+                </p>
+                <p>
+                  <small>
+                    {product.delivery.type} from {product.delivery.from.city} -{" "}
+                    {product.delivery.from.country}
+                  </small>
+                </p>
+              </section>
+              <section className="mt-5 px-3">
+                <button className="btn btn-outline-primary rounded-pill px-4 me-2">
+                  Add to Cart
+                </button>
+                <button className="btn btn-primary rounded-pill px-4">
+                  Buy Now
+                </button>
+              </section>
+            </div>
           </div>
         </section>
         <section className="mt-5 text-center">
