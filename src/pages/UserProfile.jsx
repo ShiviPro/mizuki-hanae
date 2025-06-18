c1c73dfd989274554afc9e095b358f9796bf74a5
Shivam Tewari <43674175+ShiviPro@users.noreply.github.com>
2025-06-18 23:00:16 +0530

    feat: add address manager to user profile page

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -173,10 +173,252 @@ const UserProfile = () => {
     </>
   );
 
+  const AddressManager = () => {
+    const [isNewAddressBeingAdded, setIsNewAddressBeingAdded] = useState(false);
+    const [nameIn, setNameIn] = useState("");
+    const [mobileNoIn, setMobileNoIn] = useState("");
+    const [postalCodeIn, setPostalCodeIn] = useState("");
+    const [houseNoIn, setHouseNoIn] = useState("");
+    const [localityIn, setLocalityIn] = useState("");
+    const [cityDistrictOrTownIn, setCityDistrictOrTownIn] = useState("");
+    const [stateIn, setStateIn] = useState("");
+    const [countryIn, setCountryIn] = useState("");
+    const [landmarkIn, setLandmarkIn] = useState("");
+    const [altMobileNoIn, setAltMobileNoIn] = useState("");
+    const [addressTypeIn, setAddressTypeIn] = useState("");
+    const [allAddresses, setAllAddresses] = useState(currentUser.addresses);
+
+    const addrTypeHandler = (event) => setAddressTypeIn(event.target.value);
+
+    const addrFormHandler = (event) => {
+      event.preventDefault();
+
+      const newAddress = {
+        id:
+          (currentUser.addresses[currentUser.addresses.length - 1]?.id ?? 0) +
+          1,
+        receiverName: nameIn,
+        contactNo: mobileNoIn,
+        houseNo: houseNoIn,
+        streetNameOrLocality: localityIn,
+        cityOrDistrictOrTown: cityDistrictOrTownIn,
+        landmark: landmarkIn,
+        pincode: postalCodeIn,
+        state: stateIn,
+        country: countryIn,
+        altContactNo: altMobileNoIn,
+        type: addressTypeIn,
+      };
+
+      setAllAddresses([...allAddresses, newAddress]);
+      currentUser.addresses = [...allAddresses, newAddress];
+      setIsNewAddressBeingAdded(false);
+    };
+
+    const deleteAddress = (event) => {
+      const addressId = event.target.getAttribute("data-addr-id");
+
+      setAllAddresses(
+        allAddresses.filter((address) => address.id != addressId)
+      );
+      currentUser.addresses = allAddresses.filter(
+        (address) => address.id != addressId
+      );
+    };
+
+    return (
+      <>
+        <h2 className="mb-4">Manage Addresses</h2>
+        {isNewAddressBeingAdded || (
+          <button
+            className="btn btn-outline-primary"
+            onClick={() => setIsNewAddressBeingAdded(true)}
+          >
+            ADD A NEW ADDRESS
+          </button>
+        )}
+        {isNewAddressBeingAdded && (
+          <section className="bg-primary-subtle p-4">
+            <h6 className="text-primary fw-light">ADD A NEW ADDRESS</h6>
+            <form className="col-9" onSubmit={addrFormHandler}>
+              <div className="row g-2">
+                <div className="col-12">
+                  <input
+                    placeholder="Country"
+                    className="form-control"
+                    required
+                    onChange={(event) => setCountryIn(event.target.value)}
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    placeholder="Name"
+                    className="form-control"
+                    onChange={(event) => setNameIn(event.target.value)}
+                    required
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    type="tel"
+                    placeholder="10-digit mobile number"
+                    className="form-control"
+                    onChange={(event) => setMobileNoIn(event.target.value)}
+                    required
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    placeholder="Postal Code"
+                    className="form-control"
+                    onChange={(event) => setPostalCodeIn(event.target.value)}
+                    required
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    placeholder="House Number"
+                    className="form-control"
+                    onChange={(event) => setHouseNoIn(event.target.value)}
+                    required
+                  />
+                </div>
+                <div className="col-12">
+                  <textarea
+                    placeholder="Locality (Street and Area)"
+                    className="form-control"
+                    onChange={(event) => setLocalityIn(event.target.value)}
+                    required
+                  ></textarea>
+                </div>
+                <div className="col-md-6">
+                  <input
+                    placeholder="City/District/Town"
+                    className="form-control"
+                    onChange={(event) =>
+                      setCityDistrictOrTownIn(event.target.value)
+                    }
+                    required
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    placeholder="State"
+                    className="form-control"
+                    onChange={(event) => setStateIn(event.target.value)}
+                    required
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    placeholder="Landmark (Optional)"
+                    className="form-control"
+                    onChange={(event) => setLandmarkIn(event.target.value)}
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    type="tel"
+                    placeholder="Alternate Phone (Optional)"
+                    className="form-control"
+                    onChange={(event) => setAltMobileNoIn(event.target.value)}
+                  />
+                </div>
+                <div className="col-md-6 mt-3">
+                  <label htmlFor="form-label">Address Type</label>
+                  <br />
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="addrType"
+                    value="Home"
+                    id="homeAddrType"
+                    onChange={addrTypeHandler}
+                  />{" "}
+                  <label className="form-check-label" htmlFor="homeAddrType">
+                    Home
+                  </label>
+                  <input
+                    className="form-check-input ms-4"
+                    type="radio"
+                    name="addrType"
+                    value="Work"
+                    id="workAddrType"
+                    onChange={addrTypeHandler}
+                  />{" "}
+                  <label className="form-check-label" htmlFor="workAddrType">
+                    Work
+                  </label>
+                </div>
+              </div>
+              <div className="col-md-10 mt-3">
+                <div className="row">
+                  <div className="d-grid col-6">
+                    <button type="submit" className="btn btn-primary px-3 py-2">
+                      SAVE
+                    </button>
+                  </div>
+                  <div className="d-grid col-6">
+                    <button
+                      className="btn btn-outline-primary px-3 py-2"
+                      onClick={() => setIsNewAddressBeingAdded(false)}
+                    >
+                      CANCEL
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </section>
+        )}
+
+        <ul className="mt-4 list-unstyled">
+          <div className="row g-3">
+            {allAddresses.map((address) => (
+              <div key={address.id} className="col-md-6">
+                <li className="p-3 bg-white rounded">
+                  {address.type && (
+                    <span
+                      className="bg-secondary-subtle py-1 px-2 rounded text-secondary fw-bold"
+                      style={{ fontSize: "12.5px" }}
+                    >
+                      <small>{address.type.toUpperCase()}</small>
+                    </span>
+                  )}
+                  <h5 className="mt-2">
+                    {address.receiverName}{" "}
+                    <span className="ms-4">{address.contactNo}</span>
+                  </h5>
+                  <p>
+                    {address.houseNo}, {address.streetNameOrLocality},{" "}
+                    {address.cityOrDistrictOrTown}, {address.state},{" "}
+                    {address.country} -{" "}
+                    <span className="fw-bold">{address.pincode}</span>
+                  </p>
+
+                  <button className="btn btn-outline-primary me-3">
+                    Update
+                  </button>
+                  <button
+                    className="btn btn-outline-danger"
+                    data-addr-id={address.id}
+                    onClick={deleteAddress}
+                  >
+                    <span className="bi bi-trash-fill"></span>
+                  </button>
+                </li>
+              </div>
+            ))}
+          </div>
+        </ul>
+      </>
+    );
+  };
+
   return (
     <>
       <Header />
-      <main className="mt-5 pt-5 bg-body-tertiary">
+      <main className="mt-5 pt-5 bg-body-tertiary pb-3">
         <div className="container">
           <aside>
             <section className="bg-white">
@@ -242,7 +484,7 @@ const UserProfile = () => {
             ) : sidebarSelection === "orders" ? (
               <OrderHistory />
             ) : (
-              <>Your Addresses</>
+              <AddressManager />
             )}
           </section>
         </div>
